471f5efabbbe13d743c76bd9eb27bee4797c2523
liuao0330 <2261914136@qq.com>
2023-11-03 22:02:43 +0800

    文献主页（作者部分json格式有问题，目前没放，美化不完善）

---
+++ code/front_end/vue-system/src/views/PagePaper.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="paper-details">
+      <h2>{{ paper['*title'] }}</h2>
+      <!-- <p>Authors: {{ formatAuthors() }}</p> -->
+      <p>Abstract:</p>
+      <p class="abstract">{{ paper['*abstract'] }}</p>
+      <p>Publication Date: {{ paper['*date'] }} {{ paper['*year'] }}</p>
+      <p>DOI: <a :href="paper['*doi']" target="_blank">{{ paper['*doi'] }}</a></p>
+      <p>Venue: {{ paper['*venue'] }}</p>
+      <!-- <p>Year: {{ paper['*year'] }}</p> -->
+      <p>PDF: <a :href="paper['*pdf']" target="_blank">Download PDF</a></p>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted } from 'vue';
+  import { ElMessage } from 'element-plus';
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        paper: {
+          '*title': '',
+          '*authors': [],
+          '*abstract': '',
+          '*date': '',
+          '*doi': '',
+          '*venue': '',
+          '*year': '',
+          '*pdf': '',
+        },
+      };
+    },
+    mounted() {
+      this.getPaperDetails();
+    },
+    methods: {
+      getPaperDetails() {
+        const paperId = this.$route.params.paper_id;
+        const url = `http://127.0.0.1:5000/information/papers/${paperId}`;
+        axios.get(url)
+          .then((response) => {
+            //ElMessage.success('登录成功');
+            this.paper = response.data;
+
+          })
+          .catch((error) => {
+            //ElMessage.success('失败');
+            console.error('Failed to fetch paper details:', error);
+          });
+      },
+      formatAuthors() {
+        return this.paper['*authors'].map((author) => {
+          const authorObj = JSON.parse(author);
+          return authorObj['name'];
+        }).join(', ');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .paper-details {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f8f8f8;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  
+  h2 {
+    font-size: 24px;
+  }
+  
+  p {
+    margin: 10px 0;
+  }
+  
+  .abstract {
+    white-space: pre-line;
+  }
+  </style>
+  
